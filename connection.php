6435efffb1635bc12f5891bfb065053b632d38fb
ARNAV KAUSHIK <83583041+Arnavkshk@users.noreply.github.com>
2025-02-19 22:07:21 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "perfumes";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully to paradyse perfumes";
+?>
